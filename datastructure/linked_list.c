8d48ba5e357e97c5723db0070f53716e06d3b3f1
kangbumkyu <g.kangbumkyu@gmail.com>
2022-03-22 20:41:46 +0900

    Add insert_after in linked_list

--- datastructure/linked_list.c
+++ datastructure/linked_list.c
@@ -38,10 +38,25 @@ void insert_front(node_t** node, int n) {
     *node = new_node;
 }
 
+void insert_after(node_t* node, int n, int after) {
+    node_t* pp = node;
+
+    while (pp != NULL) {
+        if (pp->value == after) {
+            node_t* new_node = malloc(sizeof(node_t));
+            new_node->value = n;
+            new_node->next = pp->next;
+            pp->next = new_node;
+            break;
+        }
+        pp = pp->next;
+    }
+}
+
 void remove_node(node_t** node, int n) {
     node_t** pp = node;
 
-    while (*pp != NULL) {
+    while(*pp != NULL) {
         if ((*pp)->value == n) {
             node_t* temp = *pp;
             *pp = (*pp)->next;
@@ -59,8 +74,13 @@ int main(void) {
     insert_front(&head, 2);
     insert_front(&head, 1);
     print_list(head);
-    printf("%d\n", head->value);
+
     remove_node(&head, 2);
+    print_list(head);
+    
+    insert_after(head, 2, 1);
+    insert_after(head, 4, 3);
+    insert_after(head, 10, 5);
     print_list(head);
 
     destroy_list(head);
